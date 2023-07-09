90cd3640111190dc5e26f55b8f63b9921c51e4e4
hguetaifi <122170712+hguetaifi@users.noreply.github.com>
2023-07-09 18:53:09 +0200

    Add files via upload

---
+++ Data/TourPlannerDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TourPlanner.Models;
+
+namespace TourPlanner.Data
+{
+    public class TourPlannerDbContext : DbContext
+    {
+        public TourPlannerDbContext(DbContextOptions<TourPlannerDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Tour> Tours { get; set; }
+        public DbSet<TourLog> TourLogs { get; set; }
+    }
+
+}
